a5f18ab549e5fbacc964dc4520698b682c6f1542
HarneetKaur04 <harneetsaini40@gmail.com>
2022-11-03 13:07:45 -0700

    cheched info displaying correctly and user authentication works

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -7,22 +7,18 @@ const Profile = (props) => {
 
   return (
     <div>
-      <div className="user_img">
-      <img src="https://static6.depositphotos.com/1010340/585/v/600/depositphotos_5859083-stock-illustration-panda-cartoon.jpg"
-            alt="Profile"
-            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-          />
+      <div className='user_profile'>
+        <div className="user_img">
+          <img src="https://static6.depositphotos.com/1010340/585/v/600/depositphotos_5859083-stock-illustration-panda-cartoon.jpg"
+                alt="Profile"
+            />
+        </div>
         <div className="user_details">
           <h2>First Name: {user.given_name}</h2>
-        </div>
-        <div className="user_details">
           <h2>Last Name: {user.family_name}</h2>
-        </div>
-        <div className="user_details">
           <h2>Email: {user.email}</h2>
         </div>
-      </div>
-      
+      </div>  
     </div>
   );
 };
