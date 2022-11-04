59963fef53bd33b057af26449bf7a5935967ae28
HarneetKaur04 <harneetsaini40@gmail.com>
2022-11-03 17:58:56 -0700

    set up backend end points for activities

---
+++ server/routes/activities.js
@@ -0,0 +1,50 @@
+// import db from "../db/db-connection.js";
+import Router from "express";
+import { config } from "dotenv";
+config();
+
+
+const router = Router();
+
+//get all activities to display by fetching from national park API
+router.get("/", async (req, res) => {
+  try {
+// fetching data from API for all parks and setting limit to 100
+    let allactivitiessurl = `https://developer.nps.gov/api/v1/parks?parkCode=&limit=150&sort=&api_key=${process.env.REACT_APP_API_KEY}`
+    console.log(allactivitiessurl, "allactivitiessurl")
+
+    await fetch(allactivitiessurl)
+    .then(response => response.json())
+      // console.log(response)
+    .then(data => res.send(data)) 
+      } 
+  catch (e) {
+    console.log(e)
+    return res.status(400).json({ e });
+  }
+});
+
+// end point for single park selected by user. params will change to user input through http request
+// router.get("/:parkCode", async (req, res) => {
+//   try {
+//     const parkCode = req.params.parkCode
+//     console.log(parkCode, "parkCodeBackend")
+
+//     let singleParksurl = `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&limit=1&sort=&api_key=${process.env.REACT_APP_API_KEY}`
+//     // console.log(singleParksurl, "singleParksurl")
+
+//     await fetch(singleParksurl)
+//     .then(response => response.json())
+//       // console.log(response)
+//     .then(data => {
+//       res.send(data)
+//       console.log(data)}) 
+//       } 
+//   catch (e) {
+//     console.log(e)
+//     return res.status(400).json({ e });
+//   }
+// });
+
+
+export default router;
