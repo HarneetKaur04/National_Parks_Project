6d41254de7d99ff257c5acaa667710b7b4f0d88e
HarneetKaur04 <harneetsaini40@gmail.com>
2022-11-11 16:49:15 -0800

    testing ToggleFavorite Button

--- client/src/components/__tests__/header.test.js
+++ client/src/components/__tests__/header.test.js
@@ -1,4 +1,5 @@
-import { screen, render, fireEvent } from "@testing-library/react";
+import { screen, render, fireEvent, configure } from "@testing-library/react";
+import USAMap from "react-usa-map";
 import "@testing-library/jest-dom";
 import Header from "../Header"
 
@@ -9,4 +10,13 @@ describe('Image', () => {
       expect(image).toHaveAttribute('src', 'https://img5.goodfon.com/wallpaper/nbig/1/da/ray-bilcliff-by-ray-bilcliff-beautiful-dawn-evening-landscap.jpg');
       expect(image).toHaveAttribute('alt', 'mountains');
     });
+
+    // test("should call mapHandler callback when clicked on USA MAP", () => {
+    //     const event = jest.fn();
+    //     const { getByTestId } = render(<USAMap onClick={event}/>);
+    //     const inputEventElement = getByTestId('content-input3');
+    //     fireEvent.click(inputEventElement);
+    //     expect(event).toHaveBeenCalled();
+    //   });
+
   });
