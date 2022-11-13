f15deeada89fe5f9c49de53e6232a551d04a2d9c
HarneetKaur04 <harneetsaini40@gmail.com>
2022-11-13 00:06:04 -0800

    video adjusted

--- client/src/components/ParkVideo.js
+++ client/src/components/ParkVideo.js
@@ -7,7 +7,7 @@ const ParkVideo = () => {
         <div className="video_space">
         <ReactPlayer style={{margin: 'auto'}} url="https://www.youtube.com/watch?v=KcI_xfryMD0"/>
         </div>
-        <div className="video_space">
+        <div className="video_space2">
             <h3 className="video_heading">Our Great National Parks</h3>
             <p className="video_paragraph">Celebrate and discover the power of our planet’s greatest national parks and wild spaces. <br/> OUR GREAT NATIONAL PARKS beckons us to get out and explore, create new ways for these wild places to thrive, and vigorously preserve them for future generations to come. </p>
         </div>
